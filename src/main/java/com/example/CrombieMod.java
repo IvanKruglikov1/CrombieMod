7abecedc398ccf4e2351e14815b69ce87eafc828
MrBaton <153443828+IvanKruglikov1@users.noreply.github.com>
2025-06-19 09:46:31 +0300

    Add files via upload

---
+++ src/main/java/com/example/CrombieMod.java
@@ -0,0 +1,10 @@
+package com.example.crombiemod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CrombieMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("CrombieMod загружен!");
+    }
+}
